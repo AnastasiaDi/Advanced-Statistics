238ba59837ef194a3a6dee9a7902eb3848df8014
meikemorren <meikemorren@hotmail.com>
2016-03-29 13:54:54 +0200

    Create exe2_1.r

---
+++ exe/Lecture2/exe2_1.r
@@ -0,0 +1,36 @@
+
+#################################################
+################# crosstable  #################
+#################################################
+
+# how many females have a high education?
+
+
+# within the Netherlands?
+
+
+#################################################
+################### recode  ####################
+#################################################
+
+# recode job variable into a lower number of logical categories
+# contains following 19 categories
+catNames <- c("Missing; Unkown","Not asked in survey","Not applicable","No answer","Don´t know",
+              "Employer/manager of establishment with 10 or more employed","Employer/manager of establishment with less than 10 employed"
+              "Professional worker","Supervisory Non manual -office worker","Non manual -office worker","Foreman and supervisor"
+              "Skilled manual","Semi-skilled manual worker","Unskilled manual","Farmer: has own farm","Agricultural worker"
+              "Member of armed forces","Never had a job","other")
+
+# 1. create new variable 
+
+
+# 2. select the employers
+
+
+# 3. assign new value (between 1 and 10)
+
+
+# do this for all categories
+
+
+# check with crosstable
