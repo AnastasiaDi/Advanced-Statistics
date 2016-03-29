29e16616522081b612d599221b564f2d5a649d82
meikemorren <meikemorren@hotmail.com>
2016-03-29 13:54:13 +0200

    Create exe1_3.r

---
+++ exe/lecture1/exe1_3.r
@@ -0,0 +1,71 @@
+#################################################
+#################### plot ####################
+#################################################
+# create variable using seq function
+# plot the variable using plot
+
+
+#################################################
+#################### barplot ####################
+#################################################
+# make a barplot a single variable: employment status
+
+
+
+
+# stacked barplot: gender vs employment status
+# TIP1: first backtransform the gender variables
+#   into numeric variables (if you still work from the data 
+#   obtained in the previous exercise)
+#   'as.numeric(as.character(dataSet$gender))'
+
+# TIP2: change misisng values into NA (not available, see exe1_2.r)
+#   then these values will not be taken into account
+
+
+
+# OPTIONAL: add label names to x-axis, vertically
+
+
+
+#################################################
+#################### histogram ####################
+#################################################
+# NOTE. this is not a test of normality!
+
+# create a histogram of income
+# use function hist
+
+
+
+# test the normality
+# use functions 'skewness/kurtosis'
+
+
+
+# how is the variable distributed?
+# TIP: did you exclude the missing values?
+
+
+#################################################
+#################### boxplot ####################
+#################################################
+
+# TIP: to split by groups, a factor variable is useful
+# create boxplot of a normally distributed variable 
+# for three groups (hint: matrix)
+
+# create normally distributed variable
+
+# set limits of boxplot
+
+
+# make boxplot
+
+
+#### OPTIONAL
+# place two boxplots next to each other
+# create another normally distr variable for three groups
+
+
+# set par values
